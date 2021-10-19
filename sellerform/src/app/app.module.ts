31bc18d6ff0386c2d88f6fbcb6bb5c3a07f11c61
AkashTandekar1 <akashta@cybage.com>
2021-10-19 10:45:00 +0530

    design functionality completed

--- sellerform/src/app/app.module.ts
+++ sellerform/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SellerComponent } from './seller/seller.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SellerComponent,
+    HeaderComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
